9fd6e960db01555f9b16af996ac08b34dc92cc16
Hayden Fernandes <haydenfernandes.ti@gmail.com>
2024-06-18 19:12:55 -0300

    Update script.js
    Foi inserido o Time da música, a barra de progressão, as funções dos botões de embaralhar, e repetir música e o storage.

--- script.js
+++ script.js
@@ -7,97 +7,158 @@ const next = document.getElementById('next');
 const previous = document.getElementById('previous');
 const currentProgress = document.getElementById('current-progress');
 const progressContainer = document.getElementById('progress-container');
+const shuffleButton = document.getElementById('shuffle');
+const repeatButton = document.getElementById('repeat');
+const songTime = document.getElementById('song-time');
+const totalTime = document.getElementById('total-time');
 
 const beloreiventar = {
-    songName : 'Reiventar',
-    artist : 'Belo',
-    file: 'belo1'
+  songName : 'Reiventar',
+  artist : 'Belo',
+  file: 'belo1'
 };
 const tiaguinhonossasala = {
-    songName : 'Nossa Sala - Hayden e Lígia',
-    artist : 'Tiaguinho',
-    file: 'tiaguinho_nossasala'
+  songName : 'Nossa Sala - Hayden e Lígia',
+  artist : 'Tiaguinho',
+  file: 'tiaguinho_nossasala'
 };
 
 const mamamiaMalu = {
-  songName : 'Dancing Queen',
-  artist : 'Abba',
-  file: 'mamamia_malu'
+songName : 'Dancing Queen',
+artist : 'Abba',
+file: 'mamamia_malu'
 };
 
 const anunciacao_Lis = {
-  songName : 'Anunciação',
-  artist : 'Mariana Nolasco',
-  file: 'anunciacao_Lis'
-};
-
+songName : 'Anunciação',
+artist : 'Mariana Nolasco',
+file: 'anunciacao_Lis'
+}; 
 
 let isPlaying = false;
-const playlist = [beloreiventar, tiaguinhonossasala, mamamiaMalu, anunciacao_Lis];
+let isShuffled = false;
+let repeatOn = false;
+const originalPlaylist = [beloreiventar, tiaguinhonossasala, mamamiaMalu, anunciacao_Lis ];
+let sortedPlaylist = [...originalPlaylist];
 let index = 0;
 
-function playSong(){
-    play.querySelector('.bi').classList.remove('bi-play-circle-fill');
-    play.querySelector('.bi').classList.add('bi-pause-circle-fill');
-    song.play();
-    isPlaying = true;
+//HABILITAR O BOTÃO DE PLAY
+function playSong() {
+  play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+  play.querySelector('.bi').classList.add('bi-pause-circle-fill');
+  song.play();
+  isPlaying = true;
+}
+// HABILITAR O BOTAO DE PAUSAR NO MESMO BOTÃO DE PLAY
+function pauseSong() {
+  play.querySelector('.bi').classList.add('bi-play-circle-fill');
+  play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+  song.pause();
+  isPlaying = false;
 }
 
-function pauseSong(){
-    play.querySelector('.bi').classList.add('bi-play-circle-fill');
-    play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
-    song.pause();
-    isPlaying = false;
-}
-
-function playPauseDecider(){
-    if(isPlaying === true){
-        pauseSong();
-    }
-    else {
-        playSong();
-    }
-}
-
-function initializeSong(){
-    cover.src = `images/${playlist[index].file}.webp`;
-    song.src = `songs/${playlist[index].file}.mp3`;
-    songName.innerText = playlist[index].songName;
-    bandName.innerText = playlist[index].artist;
-}
-
-function previousSong(){
-    if(index === 0){
-        index = playlist.length - 1;
-    }
-    else {
-        index -= 1;
-    }
-    initializeSong();
+function playPauseDecider() {
+  if (isPlaying === true) {
+    pauseSong();
+  } else {
     playSong();
+  }
 }
 
-function nextSong(){
-    if(index === playlist.length - 1){
-        index = 0;
-    }
-    else {
-        index += 1;
-    }
-    initializeSong();
+function initializeSong() {
+  cover.src = `images/${sortedPlaylist[index].file}.webp`;
+  song.src = `songs/${sortedPlaylist[index].file}.mp3`;
+  songName.innerText = sortedPlaylist[index].songName;
+  bandName.innerText = sortedPlaylist[index].artist;
+}
+
+function previousSong() {
+  if (index === 0) {
+    index = sortedPlaylist.length - 1;
+  } else {
+    index -= 1;
+  }
+  initializeSong();
+  playSong();
+}
+
+function nextSong() {
+  if (index === sortedPlaylist.length - 1) {
+    index = 0;
+  } else {
+    index += 1;
+  }
+  initializeSong();
+  playSong();
+}
+
+function updateProgress() {
+  const barWidth = (song.currentTime / song.duration) * 100;
+  currentProgress.style.setProperty('--progress', `${barWidth}%`);
+  songTime.innerText = toHHMMSS(song.currentTime);
+}
+
+function jumpTo(event) {
+  const width = progressContainer.clientWidth;
+  const clickPosition = event.offsetX;
+  const jumpToTime = (clickPosition / width) * song.duration;
+  song.currentTime = jumpToTime;
+}
+
+function shuffleArray(preShuffleArray) {
+  const size = preShuffleArray.length;
+  let currentIndex = size - 1;
+  while (currentIndex > 0) {
+    let randomIndex = Math.floor(Math.random() * size);
+    let aux = preShuffleArray[currentIndex];
+    preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
+    preShuffleArray[randomIndex] = aux;
+    currentIndex -= 1;
+  }
+}
+
+function shuffleButtonClicked() {
+  if (isShuffled === false) {
+    isShuffled = true;
+    shuffleArray(sortedPlaylist);
+    shuffleButton.classList.add('button-active');
+  } else {
+    isShuffled = false;
+    sortedPlaylist = [...originalPlaylist];
+    shuffleButton.classList.remove('button-active');
+  }
+}
+
+function repeatButtonClicked() {
+  if (repeatOn === false) {
+    repeatOn = true;
+    repeatButton.classList.add('button-active');
+  } else {
+    repeatOn = false;
+    repeatButton.classList.remove('button-active');
+  }
+}
+
+function nextOrRepeat() {
+  if (repeatOn === false) {
+    nextSong();
+  } else {
     playSong();
+  }
 }
 
-function updateProgressBar(){
-    const barWidth = (song.currentTime/song.duration)*100;
-    currentProgress.style.setProperty('--progress', `${barWidth}%`);
+function toHHMMSS(originalNumber) {
+  let hours = Math.floor(originalNumber / 3600);
+  let min = Math.floor((originalNumber - hours * 3600) / 60);
+  let secs = Math.floor(originalNumber - hours * 3600 - min * 60);
+
+  return `${hours.toString().padStart(2, '0')}:${min
+    .toString()
+    .padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
 }
 
-function jumpTo(event){
-    const width = progressContainer.clientWidth;
-    const clickPosition = event.offsetX;
-    const jumpToTime = (clickPosition/width)* song.duration;
-    song.currentTime = jumpToTime;
+function updateTotalTime() {
+  totalTime.innerText = toHHMMSS(song.duration);
 }
 
 initializeSong();
@@ -105,6 +166,9 @@ initializeSong();
 play.addEventListener('click', playPauseDecider);
 previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
-song.addEventListener('timeupdate', updateProgressBar);
+song.addEventListener('timeupdate', updateProgress);
+song.addEventListener('ended', nextOrRepeat);
+song.addEventListener('loadedmetadata', updateTotalTime);
 progressContainer.addEventListener('click', jumpTo);
-
+shuffleButton.addEventListener('click', shuffleButtonClicked);
+repeatButton.addEventListener('click', repeatButtonClicked);
